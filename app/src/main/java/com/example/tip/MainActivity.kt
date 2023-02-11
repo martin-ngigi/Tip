ec2532dc1a02b6ebeeac751b70fcd6101877af1e
martin-ngigi <martinwainaina001@gmail.com>
2023-02-11 12:40:00 +0300

    Calculate amount success"

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.tip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tip.ui.theme.TipTheme
 
@@ -22,22 +25,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TipScreen()
                 }
             }
         }
     }
 }
 
+//fun cal(number: Int): String =
+//     (0.15 * number).toString()
+
+
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TipScreen() {
+    var amount by remember { mutableStateOf("") }
+    var tip by remember { mutableStateOf("") }
+
+    fun updateAmount(s: String){
+        amount =s
+    }
+
+    fun cal(num: Double){
+        //return 15.div(100).times(num).toString()
+        tip = "KES" + num.times(0.15)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Tip Calculator")
+        TextField(
+            value = amount,
+            onValueChange = {amount =it},
+            keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Button(onClick = {
+            cal(amount.toDouble())
+        }) {
+            Text(text = "Generate")
+        }
+        Text(text = tip)
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipTheme {
-        Greeting("Android")
+        TipScreen()
     }
 }
